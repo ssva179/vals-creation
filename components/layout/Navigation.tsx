2de09bb5195d06865b79a182b6479385418be9bd
Stephanie Argueta <ssva@bu.edu>
2026-07-10 01:16:46 -0500

    adding placeholder info and buiding basic page.tsx

---
+++ components/layout/Navigation.tsx
@@ -0,0 +1,7 @@
+export default function Navigation() {
+    return (
+        <section>
+            Nav
+        </section>
+    );
+}
---
+++ components/sections/GalleryPreview.tsx
@@ -0,0 +1,7 @@
+export default function GalleryPreview() {
+    return (
+        <section>
+            GalleryPreview
+        </section>
+    );
+}
